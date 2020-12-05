9ec7bdc4a451c848d32f586a3229e3824e447723
Voratham (Dream) Siriapiwattanakul <dream07312@gmail.com>
2020-12-05 21:08:01 +0700

    Exmaple use rune and loop string utf8

--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
-	// []byte, byte = unit8 (0 - 255)
-	str := "Hello World"
-	firstLetter := str[0]
-	fmt.Println(firstLetter) // ASCII => H
+	// rune => int32 = unicode
+	str := "สวัสดีชาวโลก"
 
-	// we cannot set variable str[0] = "A" because It is byte
+	// ch := "ส"
+	// fmt.Println(([]byte(ch)))
 
-	fmt.Println("after convert : ", string(firstLetter))
-	fmt.Println("len", len(str))
+	word := []rune(str)
+	fmt.Println("rune", word[0], "string()", string(word[0]), "len", len(str))
+	fmt.Println("utf8 rune count in string :", utf8.RuneCountInString(str))
 
-	for i := 0; i < len(str); i++ {
-		fmt.Println(string(str[i]))
+	// we should use for range because golang will loop autu converted
+	for _, char := range str {
+		fmt.Print(string(char))
 	}
+
+	// for i := 0; i < len(str); i++ {
+	// 	fmt.Println(string(str[i]))
+	// }
 
 }
