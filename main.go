a759feef0c38f6335d2931e2bb351bf585f82954
Voratham (Dream) Siriapiwattanakul <dream07312@gmail.com>
2020-12-05 18:32:30 +0700

    How to use  map function and decalre map with make

--- main.go
+++ main.go
@@ -3,22 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	s1 := []int{1, 2, 3, 4, 5}
-	s2 := make([]int, 0, 5)
+	credits := map[string]int{
+		"Java":   3,
+		"C++":    3,
+		"Python": 4,
+	}
 
-	fmt.Println("s1 :", s1, "len(s1) :", len(s1), " cap(s1):", cap(s1))
-	fmt.Println("s2 :", s2, "len(s2) :", len(s2), " cap(s2):", cap(s2))
+	delete(credits, "Python")
+	credits["C#"] = 3
+	result, ok := credits["C#"]
+	fmt.Println(result, ok)
 
-	s2 = append(s2, 1, 2, 3, 4, 5)
-	// create s3
-	s3 := append(s2, 6)
-	fmt.Println("[2] -s2 :", s2, "len(s2) :", len(s2), " cap(s2):", cap(s2))
-	fmt.Println("s3 :", s3, "len(s3) :", len(s3), " cap(s3):", cap(s3))
+	// credits := make(map[string]int)
+	// credits["Java"] = 3
+	// credits["C++"] = 3
+	// credits["Python"] = 3
 
-	// change value from s3 poc mutate
-	s3[0] = 10
+	// result, ok := credits["Python"]
+	// fmt.Println(result, ok)
 
-	fmt.Println("[3] -s2 :", s2, "len(s2) :", len(s2), " cap(s2):", cap(s2))
-	fmt.Println("[2] -s3 :", s3, "len(s3) :", len(s3), " cap(s3):", cap(s3))
+	// fmt.Print(credits)
 
 }
