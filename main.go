6372ed7d868650a1f93cefd74b11ec62b99e7e43
Voratham (Dream) Siriapiwattanakul <dream07312@gmail.com>
2020-12-05 21:11:59 +0700

    strings library of golang utils

--- main.go
+++ main.go
@@ -2,27 +2,16 @@ package main
 
 import (
 	"fmt"
-	"unicode/utf8"
+	"strings"
 )
 
 func main() {
-	// rune => int32 = unicode
-	str := "สวัสดีชาวโลก"
+	// result := strings.Contains("BabelCoder", "bel")
+	// result := strings.Count("สวัสดี โชคดีนะ", "ดี")
+	// result := strings.HasPrefix("Hello World", "Hell")
+	// result := strings.HasSuffix("Hello World", "ld")
+	// result := strings.Join([]string{"สวัสดี", "ชาวโลก"}, "-")
+	result := strings.ToUpper("hello")
 
-	// ch := "ส"
-	// fmt.Println(([]byte(ch)))
-
-	word := []rune(str)
-	fmt.Println("rune", word[0], "string()", string(word[0]), "len", len(str))
-	fmt.Println("utf8 rune count in string :", utf8.RuneCountInString(str))
-
-	// we should use for range because golang will loop autu converted
-	for _, char := range str {
-		fmt.Print(string(char))
-	}
-
-	// for i := 0; i < len(str); i++ {
-	// 	fmt.Println(string(str[i]))
-	// }
-
+	fmt.Println("result ::", result)
 }
