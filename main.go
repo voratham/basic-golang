e2a976696166affc1d876923fd8e34c830f74504
Voratham (Dream) Siriapiwattanakul <dream07312@gmail.com>
2020-12-05 16:49:38 +0700

    How to use switch-case

--- main.go
+++ main.go
@@ -3,20 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	// for n := 1; n <= 10; n++ {
-	// 	fmt.Println("n = ", n)
-	// }
+	day := "Saturday"
 
-	// while loop ?
-	// n := 1
-	// for n <= 10 {
-	// 	fmt.Println("n = ", n)
-	// 	n = n + 1
-	// }
+	switch day {
+	case "Sunday", "Saturday":
+		fmt.Println("Weekend 😎")
 
-	// loop forever
-	for {
-		fmt.Println("Hello")
+	default:
+		fmt.Println("Workday 😭😭😭 !!")
 	}
-
 }
