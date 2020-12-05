e02521d41a2ae422b6e42c03f4a240261bd02aa5
Voratham (Dream) Siriapiwattanakul <dream07312@gmail.com>
2020-12-05 22:34:11 +0700

    Empty interface

--- main.go
+++ main.go
@@ -1,43 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
-type shape interface {
-	area() float64
-}
-
-type circle struct {
-	radius float64
-}
-
-type rectangle struct {
-	width  float64
-	height float64
-}
-
-func (c circle) area() float64 {
-	return math.Pi * math.Pow(c.radius, 2)
-}
-
-func (r rectangle) area() float64 {
-	return 2 * (r.width + r.height)
-}
+import "fmt"
 
 func main() {
-	var s shape
-	s = circle{radius: 10}
+	// var s interface{}
+	result := []interface{}{"A", false, 10}
+	fmt.Println(result)
 
-	switch s.(type) {
-	case circle:
-		fmt.Println("circle")
-	case rectangle:
-		fmt.Println("rectangle")
-	}
-
-	// s,ok = circle{radius: 10}
-	// fmt.Println("ok :", ok)
-	// fmt.Printf("%+v", c)
 }
