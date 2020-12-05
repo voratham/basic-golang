587c14820404f0b8ced467704336beb662c2126a
Voratham (Dream) Siriapiwattanakul <dream07312@gmail.com>
2020-12-05 22:03:43 +0700

    Interface basic

--- main.go
+++ main.go
@@ -2,33 +2,20 @@ package main
 
 import "fmt"
 
-type enrollment struct {
-	semaster string
-	courses  []string
+type generator interface {
+	generate()
 }
 
-// method receiver as value receiver
-func (e enrollment) courseAt(index uint) string {
-	return e.courses[index]
+type pdf struct {
+	content string
 }
 
-// method receiver as pointer receiver
-func (e *enrollment) addCourse(course string) {
-	e.courses = append(e.courses, course)
-
+func (p pdf) generate() {
+	fmt.Println("Generating...")
 }
 
 func main() {
-	e := enrollment{semaster: "1/63", courses: []string{"Java", "C#"}}
-
-	// e.courses = append(e.courses, "C++", "Javascript")
-
-	// equal (&e).addCourse("Golang") because golang complier smart and auto convert
-	(e).addCourse("Golang")
-
-	result := e.courseAt(0)
-	fmt.Println("index ::", result)
-
-	fmt.Printf("%+v", e)
-
+	var gen generator
+	gen = pdf{content: "My PDF"}
+	gen.generate()
 }
