085590d7a517585dd7edcfffa86e6ad8945edd06
Voratham (Dream) Siriapiwattanakul <dream07312@gmail.com>
2020-12-05 16:36:11 +0700

    const and iota declare

--- main.go
+++ main.go
@@ -3,13 +3,28 @@ package main
 import "fmt"
 
 func main() {
+	// const (
+	// 	red   = 0
+	// 	green = 1
+	// 	blue  = 2
+	// )
 
-	// var i int
-	// i = 20
+	// const (
+	// 	red = iota + 1
+	// 	green
+	// 	blue
+	// )
 
-	i := 20.02
-	j := int(i)
+	// fmt.Println(red, green, blue)
 
-	fmt.Println(i)
-	fmt.Println(j)
+	const (
+		sun = iota + 1
+		mon
+		tue
+		_
+		_
+		_
+		sat
+	)
+	fmt.Println(sun, mon, tue, sat)
 }
