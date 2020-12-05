89ef3d72944d43f958735a5a566bb3723fba31e9
Voratham (Dream) Siriapiwattanakul <dream07312@gmail.com>
2020-12-05 17:00:42 +0700

    How to use function and return

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import "fmt"
 
+func add(a, b int) int {
+	return a + b
+
+}
+
+func next(num int) (int, int) {
+	return num, num + 1
+}
+
 func main() {
-	day := "Saturday"
-
-	switch day {
-	case "Sunday", "Saturday":
-		fmt.Println("Weekend 😎")
-
-	default:
-		fmt.Println("Workday 😭😭😭 !!")
-	}
+	result := add(1, 1)
+	current, next := next(1)
+	fmt.Println("result ::", result)
+	fmt.Println(current, next)
 }
