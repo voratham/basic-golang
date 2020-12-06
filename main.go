761c9db61d846b0ae720e461f4adf6d0f2916347
Voratham (Dream) Siriapiwattanakul <dream07312@gmail.com>
2020-12-06 21:08:15 +0700

    Gorouting and channel exmaple

--- main.go
+++ main.go
@@ -2,25 +2,25 @@ package main
 
 import "fmt"
 
-func f() {
-	panic("from f")
-}
-
-func g() {
-	f()
-}
-
-func h() {
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println(r, "Recover")
-
-		}
-	}()
-
-	g()
+func sum(ch chan int, nums []int) {
+	result := 0
+	for _, n := range nums {
+		result += n
+	}
+	ch <- result
 }
 
 func main() {
-	h()
+	ch := make(chan int)
+	result := 0
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000}
+
+	go sum(ch, nums[0:10])
+	go sum(ch, nums[10:20])
+	go sum(ch, nums[20:])
+
+	for count := 0; count < 3; count++ {
+		result += <-ch
+	}
+	fmt.Println("final result :", result)
 }
